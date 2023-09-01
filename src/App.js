a308efc355c7b04cb9399028865eea6076872428
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2023-09-01 10:57:58 +0900

    added people
    Added users to site.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Person = (props) => {
+  return (
+    <>
+    <h1>Name: {props.name}</h1>
+    <h2>Last Name: {props.lastName}</h2>
+    <h2>age: {props.age}</h2>
+    </>
+  )
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Person name={"Jay"} lastName={"Dub"} age={"34"}/>
+        <Person name={"Sarah"} lastName={"Miller"} age={"34"}/>
+        <Person name={"Ryu"} lastName={"Hazuka"} age={"37"}/>
+        <Person name={"Jidion"} lastName={"Richards"} age={"23"}/>
+        <Person name={"KSI"} lastName={"Tunde"} age={"14"}/>
+        <Person name={"Logan"} lastName={"Paul"} age={"4"}/>
     </div>
   );
 }

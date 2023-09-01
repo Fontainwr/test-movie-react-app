ce93ca5d37dba6463ebab97cebc5128f4d78df60
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2023-09-01 11:06:52 +0900

    Added counter
    Added the counter function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 
-const Person = (props) => {
-  return (
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: {props.lastName}</h2>
-    <h2>age: {props.age}</h2>
-    </>
-  )
-}
-
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-        <Person name={"Jay"} lastName={"Dub"} age={"34"}/>
-        <Person name={"Sarah"} lastName={"Miller"} age={"34"}/>
-        <Person name={"Ryu"} lastName={"Hazuka"} age={"37"}/>
-        <Person name={"Jidion"} lastName={"Richards"} age={"23"}/>
-        <Person name={"KSI"} lastName={"Tunde"} age={"14"}/>
-        <Person name={"Logan"} lastName={"Paul"} age={"4"}/>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
     </div>
   );
 }
